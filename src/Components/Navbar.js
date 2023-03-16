19424dda43dd199c88c21aa4e5d211df2887fd47
seavalanche <seavalanche@gmail.com>
2023-03-17 02:39:17 +0700

    updated responsive design

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,29 @@
 import logo from '../Assets/Logo.png';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <div className='navbar'>
             <div className='navbar-wrapper'>
-                <NavLink to='/'><img className='logo' activeClassname='active' src={logo} alt="Seavalanche logo"/></NavLink>
-                {/* <div className='navbar-menu'>
-                    <Link to='/Projects' className='navbar-menu-text'>Projects</Link>
-                    <Link to='/Commissions' className='navbar-menu-text'>Commissions</Link>
-                    <Link to='/About' className='navbar-menu-text'>About</Link>
-                </div> */}
-                <div className='navbar-menu'>
+                <NavLink to='/'><img className='logo' activeClassname='active' src={logo} alt="Seavalanche logo" /></NavLink>
+                <div className='navbar-menu-toggle' onClick={handleMenuToggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
                     <NavLink to='/Projects' className='navbar-menu-text' activeClassname='active'>Projects</NavLink>
                     <NavLink to='/Commissions' className='navbar-menu-text' activeClassname='active'>Commissions</NavLink>
                     <NavLink to='/About' className='navbar-menu-text' activeClassname='active'>About</NavLink>
                 </div>
-                <div className='navbar-func'>
+                <div className={`navbar-func ${menuOpen ? 'open' : ''}`}>
                     <div className='navbar-func-text'>Dark Theme</div>
                     <div className='navbar-func-text'>EN / JP</div>
                 </div>
