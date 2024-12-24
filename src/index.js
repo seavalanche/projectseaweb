b388aea3cbc2818097448b8288fc4409fc993831
vesnea <104858535+seavalanche@users.noreply.github.com>
2024-12-24 21:14:17 +0700

    check layout is still broken 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from "./ThemeContext";
+// import { ThemeProvider } from "./ThemeContext";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
+    {/* <ThemeProvider> */}
       <App />
-    </ThemeProvider>
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
 
