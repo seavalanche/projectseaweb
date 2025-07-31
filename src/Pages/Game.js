1754b34499225b28ff330ed30a543fd1d4ea23e1
vesnea <104858535+seavalanche@users.noreply.github.com>
2025-08-01 06:37:15 +0700

    update the test game tictactoe

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -1,15 +1,53 @@
-function GodotGame() {
+export default function GamePage() {
     return (
-        <div style={{ width: "100%", height: "600px", border: "none" }}>
-            <iframe
-                title="Tic Tac Toe Game"
-                src={`${process.env.PUBLIC_URL}/game/TicTacToe.html`}
-                width="100%"
-                height="100%"
-                style={{ border: 'none' }}
-            ></iframe>
+        <div className='tictactoe'>
+            <div
+                style={{
+                    width: "600px",
+                    height: "600px",
+                    border: "none",
+                }}
+            >
+                <iframe
+                    title="Tic Tac Toe Game"
+                    src={`${process.env.PUBLIC_URL}/game/TicTacToe.html`}
+                    // width="100%"
+                    // height="100%"
+                    style={{ border: 'none' }}
+                    width="100%"
+                    height="100%"
+                ></iframe>
+            </div>
         </div>
     );
 }
+// import { useEffect } from "react";
 
-export default GodotGame
+// export default function GamePage() {
+//     useEffect(() => {
+//         if (!document.getElementById("godot-script")) {
+//             const script = document.createElement("script");
+//             script.id = "godot-script";
+//             script.src = `${process.env.PUBLIC_URL}/game/TicTacToe.js`;
+//             script.async = true;
+//             document.body.appendChild(script);
+//         }
+//     }, []);
+
+//     return (
+//         <div
+//             id="godot-container"
+//             style={{
+//                 width: "100vw",
+//                 height: "100vh",
+//                 overflow: "hidden",
+//                 backgroundColor: "#000", // or your choice
+//                 display: "flex",
+//                 justifyContent: "center",
+//                 alignItems: "center",
+//             }}
+//         >
+//             <canvas id="canvas"></canvas>
+//         </div>
+//     );
+// }
